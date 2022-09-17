fb3312514b8d6c6103c8effc228391ba2f0337f4
Richard JEON <ztzy1907@gmail.com>
2022-09-18 03:51:24 +0900

    feat: 프론트 환경설정별 깃헙 로그인 URL 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
+                <button type="button"><a href={process.env.REACT_APP_OAUTH_URL}>Sign in with Github</a></button>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
@@ -15,7 +16,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React + test
+                    Learn React + test11
                 </a>
             </header>
         </div>
