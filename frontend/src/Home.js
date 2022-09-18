8e9dc11445eb9c18d69dae26298b96b505483c57
Richard JEON <ztzy1907@gmail.com>
2022-09-19 03:04:33 +0900

    refactor: 프론트 메인 페이지 간소화

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,23 +2,11 @@ function Home() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
                 <button type="button">
                     <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_OAUTH_CLIENT_ID}`}>
                         Sign in with Github
                     </a>
                 </button>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React + test11
-                </a>
             </header>
         </div>
     );
