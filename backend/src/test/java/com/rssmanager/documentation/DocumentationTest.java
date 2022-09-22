db05247a01eee40704f28364b0404446e3f9146a
Richard JEON <ztzy1907@gmail.com>
2022-09-22 18:43:32 +0900

    feat: RestDocs 설정 및 RSS 조회 문서화 테스트 구현

---
+++ backend/src/test/java/com/rssmanager/documentation/DocumentationTest.java
@@ -0,0 +1,49 @@
+package com.rssmanager.documentation;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import com.rssmanager.auth.service.AuthService;
+import com.rssmanager.member.service.MemberService;
+import com.rssmanager.rss.service.FeedService;
+import com.rssmanager.rss.service.RssService;
+import com.rssmanager.util.SessionManager;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest
+public class DocumentationTest {
+
+    @MockBean
+    protected RssService rssService;
+    @MockBean
+    protected AuthService authService;
+    @MockBean
+    protected MemberService memberService;
+    @MockBean
+    protected FeedService feedService;
+    @MockBean
+    protected SessionManager sessionManager;
+    protected MockMvcRequestSpecification docsGiven;
+
+    @BeforeEach
+    void setDocsGiven(final WebApplicationContext webApplicationContext,
+                      final RestDocumentationContextProvider restDocumentation) {
+        docsGiven = RestAssuredMockMvc.given()
+                .mockMvc(MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                        .apply(documentationConfiguration(restDocumentation)
+                                .operationPreprocessors()
+                                .withRequestDefaults(prettyPrint())
+                                .withResponseDefaults(prettyPrint()))
+                        .build()).log().all();
+    }
+}
---
+++ backend/src/test/java/com/rssmanager/documentation/RssControllerTest.java
@@ -0,0 +1,53 @@
+package com.rssmanager.documentation;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+
+import com.rssmanager.rss.domain.Rss;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+class RssControllerTest extends DocumentationTest {
+
+    @Test
+    void RSS_목록_조회() {
+        when(rssService.findAll()).thenReturn(
+                List.of(
+                        new Rss(1L, "화음을 좋아하는 리차드", "https://creampuffy.tistory.com/rss",
+                                "https://creampuffy.tistory.com", "", false),
+                        new Rss(2L, "D2 Blog", "https://d2.naver.com/d2.atom", "https://d2.naver.com",
+                                "https://d2.naver.com/favicon.ico", true),
+                        new Rss(3L, "우아한형제들 기술블로그", "https://techblog.woowahan.com/feed/",
+                                "https://techblog.woowahan.com",
+                                "https://techblog.woowahan.com/wp-content/uploads/2020/08/favicon.ico", true)
+                )
+        );
+
+        docsGiven
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/api/rss")
+                .then().log().all()
+                .apply(document("rss",
+                        responseFields(
+                                fieldWithPath("rssResponses.[].id").type(JsonFieldType.NUMBER)
+                                        .description("RSS id"),
+                                fieldWithPath("rssResponses.[].title").type(JsonFieldType.STRING)
+                                        .description("RSS title"),
+                                fieldWithPath("rssResponses.[].rssUrl").type(JsonFieldType.STRING)
+                                        .description("RSS rssUrl"),
+                                fieldWithPath("rssResponses.[].link").type(JsonFieldType.STRING)
+                                        .description("RSS link"),
+                                fieldWithPath("rssResponses.[].iconUrl").type(JsonFieldType.STRING)
+                                        .description("RSS iconUrl"),
+                                fieldWithPath("rssResponses.[].recommended").type(JsonFieldType.BOOLEAN)
+                                        .description("RSS recommended")
+                        )
+                ))
+                .statusCode(HttpStatus.OK.value());
+    }
+}
