4d7c247002faa4a9e8dc65a3202f83bf7f9c8b78
Richard JEON <ztzy1907@gmail.com>
2022-09-26 19:39:08 +0900

    feat: 피드 조회 API 응답에 description 추가

--- backend/src/main/java/com/rssmanager/rss/controller/dto/FeedResponse.java
+++ backend/src/main/java/com/rssmanager/rss/controller/dto/FeedResponse.java
@@ -12,15 +12,17 @@ public class FeedResponse {
     private final Long id;
     private final String title;
     private final String link;
+    private final String description;
     private final Date updateDate;
     private final RssResponse rss;
 
     @Builder
-    public FeedResponse(final Long id, final String title, final String link, final Date updateDate,
-                        final RssResponse rss) {
+    public FeedResponse(final Long id, final String title, final String link, final String description,
+                        final Date updateDate, final RssResponse rss) {
         this.id = id;
         this.title = title;
         this.link = link;
+        this.description = description;
         this.updateDate = updateDate;
         this.rss = rss;
     }
@@ -33,6 +35,7 @@ public class FeedResponse {
                 .id(feed.getId())
                 .title(feed.getTitle())
                 .link(feed.getLink())
+                .description(feed.getDescription())
                 .updateDate(feed.getUpdateDate())
                 .rss(rssResponse)
                 .build();
