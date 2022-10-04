6b813119c833ebb21a1b878ddfcacf98b433b51f
Richard JEON <ztzy1907@gmail.com>
2022-10-04 13:44:28 +0900

    feat: 검색, 북마크 화면 임시 페이지 구현

---
+++ frontend/src/views/Bookmarks.js
@@ -0,0 +1,21 @@
+import BottomNavBar from "../components/BottomNavBar";
+
+export default function Bookmarks(props) {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <img src="/construction.png" style={
+                        {
+                            top: '40vh',
+                            width: '100%',
+                            maxWidth: '700px'
+                        }}/>
+                </div>
+            </header>
+            <footer className="App-footer">
+                <BottomNavBar loginStatus={props.loginStatus} userInfo={props.userInfo} navIndex={props.navIndex}/>
+            </footer>
+        </div>
+    )
+}
