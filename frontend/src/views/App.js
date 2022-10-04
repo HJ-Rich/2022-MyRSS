3a833b38b4ba5e35a42dcad99bfc50273c9269b3
Richard JEON <ztzy1907@gmail.com>
2022-10-04 13:50:58 +0900

    feat: App.js 라우터 정리

--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -3,7 +3,13 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import Home from "./Home";
 import OAuthGithubCallback from "./OAuthGithubCallback";
-import Home2 from "./Home2";
+import Subscribed from "./Subscribed";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import GithubLogin from "./GithubLogin";
+import Profile from "./Profile";
+import Search from "./Search";
+import Bookmarks from "./Bookmarks";
 
 const darkTheme = createTheme({
     palette: {
@@ -12,19 +18,74 @@ const darkTheme = createTheme({
 });
 
 function App() {
+    const [loginStatus, setLoginStatus] = useState();
+    const [userInfo, setUserInfo] = useState({});
+
+    useEffect(() => {
+        axios.post(`${process.env.REACT_APP_API_HOST}/api/auth/certificate`,
+            {}, {withCredentials: true})
+            .then(({data}) => {
+                setLoginStatus(data.loggedIn);
+                setUserInfo(data.member);
+            })
+    }, [])
+
     return (
         <>
             <ThemeProvider theme={darkTheme}>
                 <CssBaseline/>
                 <BrowserRouter>
                     <Routes>
-                        <Route path="/" exact element={
-                            <Home fetchOption={''} navIndex={0}/>}>
+                        <Route path="/" exact
+                               element={
+                                   <Home
+                                       loginStatus={loginStatus}
+                                       userInfo={userInfo}
+                                       fetchOption={''}
+                                       navIndex={0}/>
+                               }>
                         </Route>
-                        <Route path="/subscribed" exact element={
-                            <Home2 fetchOption={'&subscribed=true'} navIndex={2}/>}>
+                        <Route path="/search" exact
+                               element={
+                                   <Search
+                                       loginStatus={loginStatus}
+                                       userInfo={userInfo}
+                                       fetchOption={''}
+                                       navIndex={1}/>
+                               }>
                         </Route>
-                        <Route path="/oauth/github" exact element={<OAuthGithubCallback/>}></Route>
+                        <Route path="/subscribed" exact
+                               element={
+                                   loginStatus
+                                       ? <Subscribed loginStatus={loginStatus}
+                                                     userInfo={userInfo}
+                                                     fetchOption={'&subscribed=true'}
+                                                     navIndex={2}/>
+                                       : <GithubLogin loginStatus={loginStatus} navIndex={4}/>
+                               }>
+                        </Route>
+                        <Route path="/bookmarks" exact
+                               element={
+                                   <Bookmarks
+                                       loginStatus={loginStatus}
+                                       userInfo={userInfo}
+                                       fetchOption={''}
+                                       navIndex={3}/>
+                               }>
+                        </Route>
+                        <Route path="/profile" exact
+                               element={
+                                   loginStatus
+                                       ? <Profile loginStatus={loginStatus}
+                                                  userInfo={userInfo}
+                                                  fetchOption={'&subscribed=true'}
+                                                  navIndex={4}/>
+                                       : <GithubLogin loginStatus={loginStatus} navIndex={4}/>
+                               }
+                        >
+                        </Route>
+                        <Route path="/oauth/github" exact
+                               element={<OAuthGithubCallback/>}></Route>
                     </Routes>
                 </BrowserRouter>
             </ThemeProvider>
