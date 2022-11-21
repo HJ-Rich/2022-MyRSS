724f0becb1c49412b250dd840e6f6f7e0c50da13
Richard JEON <ztzy1907@gmail.com>
2022-11-22 03:50:50 +0900

    refactor(MemberRepository): 사용되지 않는 메서드 제거

--- backend/src/main/java/com/rssmanager/member/repository/MemberRepository.java
+++ backend/src/main/java/com/rssmanager/member/repository/MemberRepository.java
@@ -5,9 +5,6 @@ import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface MemberRepository extends Repository<Member, Long> {
-
-    Optional<Member> findById(Long id);
-
     Member save(Member member);
 
     Optional<Member> findByProviderId(Long providerId);
