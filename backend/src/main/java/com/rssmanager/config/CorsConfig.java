1e7d737b6a94a293d183aee4238cc846f0e1f526
Richard JEON <ztzy1907@gmail.com>
2022-11-23 03:09:26 +0900

    refactor(CorsConfig): 로컬 개발 환경에서만 Cors 허용이 적용되도록 개선

--- backend/src/main/java/com/rssmanager/config/CorsConfig.java
+++ backend/src/main/java/com/rssmanager/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.rssmanager.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile({"test", "local"})
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
@@ -12,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dev.myrss.ga", "https://myrss.ga")
+                .allowedOrigins("http://localhost:3000")
                 .allowCredentials(true)
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .exposedHeaders(HttpHeaders.LOCATION);
